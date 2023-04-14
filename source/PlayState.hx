30aeafd858f7624d6ecc5dcc6c100ead478df9e9
portilizenHub <matthewallanvincent09@gmail.com>
2023-04-14 07:52:50 -0500

    add a floor

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,6 +11,7 @@ class PlayState extends FlxState
 {
 	var net:FlxSprite;
 	var counch:FlxSprite;
+	var floor:FlxSprite;
 
 	var offsets:Array<String> = coolTextFile(File.data('stage'));
 	var sizes:Array<String> = coolTextFile(File.data('size'));
@@ -21,6 +22,11 @@ class PlayState extends FlxState
 	{
 		trace(offsets);
 		trace(sizes);
+
+		floor = new FlxSprite(Std.parseFloat(offsets[2]), Std.parseFloat(offsets[3])).loadGraphic(File.image('floor'));
+		floor.setGraphicSize(Std.int(floor.width * (pixelZoom + Std.int(Std.parseFloat(sizes[1])))),
+			Std.int(floor.height * (pixelZoom + Std.int(Std.parseFloat(sizes[1])))));
+		add(floor);
 
 		counch = new FlxSprite(Std.parseFloat(offsets[0]), Std.parseFloat(offsets[1])).loadGraphic(File.image('counch'));
 		counch.setGraphicSize(Std.int(counch.width * (pixelZoom + Std.int(Std.parseFloat(sizes[0])))),
@@ -38,6 +44,10 @@ class PlayState extends FlxState
 	{
 		FlxG.mouse.visible = false;
 		net.setPosition(FlxG.mouse.x, FlxG.mouse.y);
+
+		floor.setPosition(Std.parseFloat(offsets[2]), Std.parseFloat(offsets[3]));
+		floor.setGraphicSize(Std.int(floor.width * (pixelZoom + Std.int(Std.parseFloat(sizes[1])))),
+			Std.int(floor.height * (pixelZoom + Std.int(Std.parseFloat(sizes[1])))));
 
 		counch.setPosition(Std.parseFloat(offsets[0]), Std.parseFloat(offsets[1]));
 		counch.setGraphicSize(Std.int(counch.width * (pixelZoom + Std.int(Std.parseFloat(sizes[0])))),

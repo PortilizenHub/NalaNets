f4d7a7dc939f49bed05ca5dc46afa62414f3e6c6
portilizenHub <matthewallanvincent09@gmail.com>
2023-04-14 07:41:19 -0500

    size file

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -13,15 +13,18 @@ class PlayState extends FlxState
 	var counch:FlxSprite;
 
 	var offsets:Array<String> = coolTextFile(File.data('stage'));
+	var sizes:Array<String> = coolTextFile(File.data('size'));
 
 	var pixelZoom:Int = 4;
 
 	override public function create()
 	{
 		trace(offsets);
+		trace(sizes);
 
 		counch = new FlxSprite(Std.parseFloat(offsets[0]), Std.parseFloat(offsets[1])).loadGraphic(File.image('counch'));
-		counch.setGraphicSize(Std.int(counch.width * pixelZoom), Std.int(counch.height * pixelZoom));
+		counch.setGraphicSize(Std.int(counch.width * (pixelZoom + Std.int(Std.parseFloat(sizes[0])))),
+			Std.int(counch.height * (pixelZoom + Std.int(Std.parseFloat(sizes[0])))));
 		add(counch);
 
 		net = new FlxSprite(FlxG.mouse.x, FlxG.mouse.y).loadGraphic(File.image('net'));
@@ -35,9 +38,13 @@ class PlayState extends FlxState
 	{
 		FlxG.mouse.visible = false;
 		net.setPosition(FlxG.mouse.x, FlxG.mouse.y);
+
 		counch.setPosition(Std.parseFloat(offsets[0]), Std.parseFloat(offsets[1]));
+		counch.setGraphicSize(Std.int(counch.width * (pixelZoom + Std.int(Std.parseFloat(sizes[0])))),
+			Std.int(counch.height * (pixelZoom + Std.int(Std.parseFloat(sizes[0])))));
 
 		offsets = coolTextFile(File.data('stage'));
+		sizes = coolTextFile(File.data('size'));
 
 		super.update(elapsed);
 	}

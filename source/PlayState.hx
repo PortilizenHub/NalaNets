44c1b1cfed8fbfca81c32f607da8d4915c3f408a
portilizenHub <matthewallanvincent09@gmail.com>
2023-04-14 07:22:19 -0500

    counch

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -7,11 +7,16 @@ import flixel.FlxState;
 class PlayState extends FlxState
 {
 	var net:FlxSprite;
+	var counch:FlxSprite;
 
 	var pixelZoom:Int = 4;
 
 	override public function create()
 	{
+		counch = new FlxSprite(120, 540).loadGraphic(File.image('counch'));
+		counch.setGraphicSize(Std.int(counch.width * pixelZoom), Std.int(counch.height * pixelZoom));
+		add(counch);
+
 		net = new FlxSprite(FlxG.mouse.x, FlxG.mouse.y).loadGraphic(File.image('net'));
 		net.setGraphicSize(Std.int(net.width * pixelZoom), Std.int(net.height * pixelZoom));
 		add(net);

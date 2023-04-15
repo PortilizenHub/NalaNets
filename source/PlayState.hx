79b69228aa20183115b0c10ed5d5fa07576af5a5
portilizenHub <matthewallanvincent09@gmail.com>
2023-04-15 07:47:40 -0500

    highscore n score n stuff n resettin n more stuf

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,9 +4,14 @@ import flixel.FlxBasic;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
+import haxe.Json;
 import openfl.Assets;
+import openfl.net.FileReference;
+import sys.FileSystem;
+import sys.io.File as SysFile;
 
 using StringTools;
 
@@ -18,21 +23,38 @@ class PlayState extends FlxState
 
 	var nala:FlxSprite;
 
+	var hiScore:FlxText;
+	var scoreT:FlxText;
+
+	var reset:FlxText;
+
 	var offsets:Array<String> = coolTextFile(File.data('stage'));
 	var sizes:Array<String> = coolTextFile(File.data('size'));
 	var nalaSettings:Array<String> = coolTextFile(File.data('nalaSettings'));
+	var scoreFile:Array<String> = coolTextFile(File.data('scoreSettings'));
 
 	var pressedNala:Bool = false;
+
+	var canReset:Bool = false;
 
 	var pixelZoom:Int = 4;
 
 	var nalaRandom:Int = 0;
+
+	var highScore:Int = 0;
+	var score:Int = 0;
+	var timer:Int = 60;
+	var timerXX:Int = 120;
 
 	override public function create()
 	{
 		trace(offsets);
 		trace(sizes);
 		trace(nalaSettings);
+		trace(scoreFile);
+
+		if (scoreFile[2] != null)
+			highScore = Std.int(Std.parseFloat(scoreFile[2]));
 
 		floor = new FlxSprite(Std.parseFloat(offsets[2]), Std.parseFloat(offsets[3])).loadGraphic(File.image('floor'));
 		floor.setGraphicSize(Std.int(floor.width * (pixelZoom + Std.int(Std.parseFloat(sizes[1])))),
@@ -57,11 +79,28 @@ class PlayState extends FlxState
 		net.setGraphicSize(Std.int(net.width * pixelZoom), Std.int(net.height * pixelZoom));
 		add(net);
 
+		hiScore = new FlxText(20, 0, 0, Std.string(highScore), 16);
+		add(hiScore);
+
+		scoreT = new FlxText(20, 40, 0, Std.string(score), 16);
+		add(scoreT);
+
+		reset = new FlxText(0, 0, 0, 'PRESS R TO RESET');
+		reset.visible = canReset;
+		reset.screenCenter();
+		reset.color = 0xff0000;
+		add(reset);
+
 		super.create();
 	}
 
 	override public function update(elapsed:Float)
 	{
+		reset.visible = canReset;
+
+		hiScore.text = Std.string(highScore);
+		scoreT.text = Std.string(score);
+
 		FlxG.mouse.visible = false;
 		net.setPosition(FlxG.mouse.x, FlxG.mouse.y);
 
@@ -79,12 +118,31 @@ class PlayState extends FlxState
 		offsets = coolTextFile(File.data('stage'));
 		sizes = coolTextFile(File.data('size'));
 		nalaSettings = coolTextFile(File.data('nalaSettings'));
-
-		if (!pressedNala)
-			pressedNala = FlxG.mouse.pressed && mouseOverlapObject(nala);
+		scoreFile = coolTextFile(File.data('scoreSettings'));
 
 		if (!pressedNala)
 		{
+			canReset = false;
+
+			pressedNala = FlxG.mouse.pressed && mouseOverlapObject(nala);
+
+			timerXX--;
+
+			if (timerXX > 1)
+			{
+				timerXX = 120;
+				timer--;
+
+				if (timer > 1)
+				{
+					timer = 60;
+					score++;
+
+					if (score > highScore)
+						highScore = score;
+				}
+			}
+
 			FlxTween.tween(nala, {x: FlxG.random.int(0, FlxG.width), y: Std.parseFloat(nalaSettings[2])}, FlxG.random.float(0.1, 2), {
 				onComplete: function(twn:FlxTween)
 				{
@@ -94,14 +152,22 @@ class PlayState extends FlxState
 		}
 		else
 		{
-			// trace('WINNER');
-			// Sys.exit(0);
+			canReset = true;
+
+			nala.setPosition(Std.parseFloat(scoreFile[0]), Std.parseFloat(scoreFile[1]));
 			nala.animation.play('idle');
+
+			if (FileSystem.exists('assets/data'))
+			{
+				SysFile.saveContent('assets/data/scoreSettings.txt', Std.string(nala.x + '\n' + nala.y + '\n' + highScore));
+			}
+
+			if (canReset)
+			{
+				if (FlxG.keys.justReleased.R)
+					FlxG.resetState();
+			}
 		}
-
-		// trace(pressedNala);
-
-		// net.animation.play(Std.string(mouseOverlapObject(floor) || mouseOverlapObject(counch)));
 
 		super.update(elapsed);
 	}
